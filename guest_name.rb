0748b6df77f45e364444ffb1913b7a93be7b07df
MegatronusPrime <nailahharvey2003@gmail.com>
2018-07-23 22:18:38 +0000

    automatically backed up by learn

--- guest_name.rb
+++ guest_name.rb
@@ -1,1 +1,12 @@
-puts "Dear #{guest_name}"
+guest_name = gets.strip
+party_name = gets.strip
+date = gets.strip
+time = gets.strip
+host_name = gets.strip
+
+puts "Dear #{guest_name},"
+
+puts "You are cordially invited to the #{party_name}" + "on #{date} at #{time}. Please make a RSVP three days before the party."
+
+puts "Sincerely,"
+puts "#{host_name}"
